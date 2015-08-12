c482f9b4a16a4f8d6edcc2d43f6c0b9777c58caa
Marius Schulz <marius.schulz@me.com>
2015-08-12 19:49:54 +0200

    Extracts glob for JS files into local variable

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,10 @@ var gulp = require("gulp");
 var babel = require("gulp-babel");
 
 var libDir = "lib/";
+var jsFiles = "src/**/*.js";
 
 gulp.task("babel", function() {
-    return gulp.src("src/**/*.js")
+    return gulp.src(jsFiles)
         .pipe(babel())
         .pipe(gulp.dest(libDir));
 });
